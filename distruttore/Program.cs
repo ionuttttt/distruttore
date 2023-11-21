ae6e01716521faf82615a1ad60d3b4fadaea271d
Ciuca.20107 <Ciuca.20107@LABI2-13PC07>
2023-11-21 10:18:15 +0100

    Aggiungere i file di progetto.

---
+++ distruttore/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace distruttore
+{
+    internal class Program
+    {
+        class esempio
+        {
+            protected string stringa;
+            protected int numero;
+
+            public esempio()
+            {
+                stringa = "ciao";
+                numero = 1;
+            }
+
+            public void stampa()
+            {
+                Console.WriteLine(stringa + numero);
+            }
+
+            ~esempio()
+            {
+                Console.WriteLine("Distruzione avvenuta");
+            }
+
+            public void dispose()
+            {
+                GC.SuppressFinalize(this);
+            }
+        }
+        static void Main(string[] args)
+        {
+            esempio es=new esempio();
+            es.stampa();
+   
+        }
+    }
+}
